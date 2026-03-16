104c891d780471d570aa6358f8006d6187a704e7
Shalesh Thakur <shalesh2283.be22@chitkara.edu.in>
2026-03-16 17:03:22 +0530

    uc1

---
+++ QuantityMeasurementApp/src/main/java/org/example/QuantityMeasurementApp.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class QuantityMeasurementApp {
+    static class Feet {
+
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null) {
+                return false;
+            }
+
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+
+            Feet other = (Feet) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Feet feet1 = new Feet(1.0);
+        Feet feet2 = new Feet(1.0);
+
+        boolean result = feet1.equals(feet2);
+
+        System.out.println("Equal (" + result + ")");
+    }
+}
